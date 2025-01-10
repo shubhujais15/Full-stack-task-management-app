0bc3e756eb37e65524d42ec9a19b51b41541d789
shubhujais15 <shubhujaisedu15@gmail.com>
2025-01-10 15:18:26 +0530

    restaurant data api

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+export const MENU_API = CORS_PROXY + "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
+export const REST_API = CORS_PROXY + "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
