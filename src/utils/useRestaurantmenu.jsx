cde70af6f71730bd8be6953f5ef67a0ba06c8d42
shubhujais15 <shubhujaisedu15@gmail.com>
2025-01-14 17:31:14 +0530

    add api handling

--- src/utils/useRestaurantmenu.jsx
+++ src/utils/useRestaurantmenu.jsx
@@ -1,20 +1,26 @@
-import {MENU_API} from "./constants"
-import { useState,useEffect } from "react";
+import { MENU_API } from "./constants";
+import { useState, useEffect } from "react";
 
 const useRestaurantmenu = (resid) => {
+    const [RestMenu, setRestMenu] = useState(null);
 
-    const [RestMenu , setRestMenu] = useState(null);
     useEffect(() => {
-        fetchMenu();
-    },[])
+        const fetchMenu = async () => {
+            try {
+                const response = await fetch(MENU_API + resid);
+                const json = await response.json();
+                setRestMenu(json.data);
+                console.log("Full Restaurant Menu Data:", json.data); // Debugging log
+            } catch (error) {
+                console.error("Error fetching data:", error.message);
+                setRestMenu("error");
+            }
+        };
 
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resid);
-        const json = await data.json();
-        console.log(json); 
-        setRestMenu(json.data)
-    };
-    return(RestMenu);
-}
+        fetchMenu();
+    }, [resid]);
+
+    return RestMenu;
+};
 
 export default useRestaurantmenu;
